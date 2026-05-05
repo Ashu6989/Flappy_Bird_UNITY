76a56bf4787aef5262913dbe05225f8d9980103e
RishiPorwal933 <rishi1026porwal@gmail.com>
2026-05-05 14:24:31 +0530

    added top pipe with gap, also the roof

--- Assets/spawnerScript.cs
+++ Assets/spawnerScript.cs
@@ -20,8 +20,15 @@ public class SpawnerScript : MonoBehaviour
     }
 
     void SpawnPipe()
-    {
-        float y = Random.Range(-2f, 2f);
-        Instantiate(pipePrefab, new Vector3(10, y, 0), Quaternion.identity);
-    }
+{
+    float gapSize = 3.8f;   // space between pipes
+    float centerY = Random.Range(-1.5f, 1.5f);
+
+    // Bottom pipe
+    Instantiate(pipePrefab, new Vector3(10, centerY - gapSize, 0), Quaternion.identity);
+
+    // Top pipe (flipped)
+    GameObject topPipe = Instantiate(pipePrefab, new Vector3(10, centerY + gapSize, 0), Quaternion.identity);
+    topPipe.transform.rotation = Quaternion.Euler(0, 0, 180);
+}
 }
